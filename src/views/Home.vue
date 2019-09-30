d95073e3f37fc044e1c378a16228abdf2f55117c
Adrien Croquelois <croquelois.adrien@gmail.com>
2019-09-30 23:37:22 +0200

    modif hello word

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Bienvenue sur le Back-Office de BUZZER"/>
   </div>
 </template>
 
