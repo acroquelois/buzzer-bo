1e5ad6c9fe67c6375a6d7d38ed85ecbed7f7f1a7
Adrien Croquelois <croquelois.adrien@gmail.com>
2019-10-20 14:46:17 +0200

    feat home vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Bienvenue sur le Back-Office de BUZZER"/>
+    <h1>Ici, tableau d'affichage des questions</h1>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
   }
 }
 </script>
