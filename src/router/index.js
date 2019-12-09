a92dd77ae1ac1cebdd7edbc55a1a70cebaff332c
Gautier couture <2017566295@lacatholille.fr>
2019-12-09 11:02:40 +0100

    first_form

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import BuzzerLayout from '../layouts/BuzzerLayout.vue'
 import Login from '../views/Login.vue'
 import Accueil from '../views/Accueil.vue'
+import Formulaire_Q_Image from '../views/Formulaire_Q_Image.vue'
 
 Vue.use(VueRouter)
 
@@ -11,6 +12,11 @@ const routes = [
     path: '/login',
     name: 'login',
     component: Login
+  },
+  {
+    path: '/fqi',
+    name: 'formulaireqi',
+    component: Formulaire_Q_Image
   },
   {
     path:'/',
---
+++ src/views/Formulaire_Q_Image.vue
@@ -0,0 +1,99 @@
+<template>
+<div>
+
+</div>
+<div class="row justify-center items-center bg-blue div_max_height">
+    <div class="col-3 column q-pa-md bg-white q-gutter-md">
+        <div>
+            <q-input
+            v-model="title"
+            label="Intitulé de la question"
+            stack-label
+            />
+        </div>
+        <div>
+            <q-uploader
+               url="http://localhost:8080/upload"
+               label="Image 1"
+               color="blue"
+               square
+               flat
+               bordered
+               style="max-width: 300px"
+             />
+         </div>
+         <div>
+             <q-uploader
+                url="http://localhost:8080/upload"
+                label="Image 2"
+                color="blue"
+                square
+                flat
+                bordered
+                style="max-width: 300px"
+              />
+        </div>
+        <div>
+              <q-uploader
+                 url="http://localhost:8080/upload"
+                 label="Image 3"
+                 color="blue"
+                 square
+                 flat
+                 bordered
+                 style="max-width: 300px"
+               />
+        </div>
+        <div>
+            <q-input
+            v-model="answer"
+            label="Réponse"
+            stack-label
+            />
+        </div>
+        <div class="row justify-end">
+            <q-btn
+            color="black"
+            label="Soumettre"
+            @click="submit"
+            />
+        </div>
+
+    </div>
+</div>
+</template>
+
+<script>
+import{
+    QInput,
+    QBtn,
+    QUploader
+} from 'quasar'
+export default {
+    name: 'Formulaire_Q_Image',
+
+    components: {
+        QInput,
+        QBtn,
+        QUploader
+    },
+
+    data () {
+        return {
+            title: 'Que représente cette image ?',
+            answer: null
+        }
+    },
+    methods:{
+        submit(){
+            this.$router.push('/accueil')
+        }
+    }
+}
+</script>
+
+<style>
+.div_max_height{
+    min-height: 100vh;
+}
+</style>
